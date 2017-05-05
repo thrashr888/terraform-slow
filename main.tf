a23784ab6fd9832870b735c20fc0f9f50cdc531b
Paul Thrasher <thrashr888@gmail.com>
2017-05-04 18:33:26 -0700

    Find instead

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "print_junk" {
   provisioner "local-exec" {
-    command = "while read line; do echo \"$line\"; sleep 0.5 ; done < /var/log/system.log"
+    command = "find /"
   }
 }
